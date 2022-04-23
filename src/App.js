346a80b28433b7d7dc3e6d80eab5f5b26fa6f01b
Holyrollwar <mrrollwar@gmail.com>
2022-04-23 20:19:14 +0300

    add meals section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
+import { Fragment } from 'react';
 import './App.css';
 import Header  from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
+    <Fragment>
     <Header></Header>
+    <main>
+      <Meals></Meals>
+    </main>
+    </Fragment>
   )
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = props => {
   return (
       <Fragment>
           <header className={classes.header}>
-            <h1>React Meals Application</h1>
+            <h1>React Meals</h1>
             <HeaderCardButton></HeaderCardButton>
           </header>
           <div className={classes['main-image']}>
